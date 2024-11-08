e4d1af0b921526a304381e8b8111cb94ccc1c7cf
jeasdigitalcollege <jorgedigitalcollege@outlook.com>
2024-11-07 21:27:19 -0300

    Alteração HTML

--- js/calcularSalario.js
+++ js/calcularSalario.js
@@ -2,16 +2,19 @@
 
 // Função Calcular Salário do Vendedor
 function calcularSalario() {
+
+    // desabilita o comportamento padrão do form
   
     // Obtendo os valores dos inputs
     let nome = document.getElementById('nome').value;
     let fixo = parseFloat(document.getElementById('fixo').value);
     let vendas = parseFloat(document.getElementById('vendas').value);
+    let comissao = parseFloat(document.getElementById('comissao').value);
 
     // Verificando se os valores são números
     if (nome !== "" && fixo !== ""  && vendas !==0) 
     {
-        salario = fixo + vendas * 0.15;
+        salario = fixo + (vendas * comissao/100);
         document.getElementById('resultado').textContent = "Valor Salário Mensal do(a) Vendedor " + nome + ": R$ " + salario.toFixed(2);
     }
     else
@@ -25,6 +28,7 @@ function limpar() {
     document.getElementById('nome').value = "";
     document.getElementById('fixo').value = "";
     document.getElementById('vendas').value = "";
+    document.getElementById('comissao').value = "";
 
     document.getElementById('resultado').textContent = "Área de Mensagens";
 }
